1712a77356c4a1f832014c422a7a15bc57a26fc8
Małgorzata Łabuzek <malgorzatalabuzek0808@gmail.com>
2024-10-30 19:07:16 +0100

    Stwory

---
+++ Simulator/Simulator/Animals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Simulator
+{
+    public class Animals
+    {
+        public required string Description { get; init; }
+        public uint Size { get; set; } = 3;
+
+        public void Info()
+        {
+            Console.WriteLine($"{Description} <{Size}>");
+        }
+    }
+}
---
+++ Simulator/Simulator/Creature .cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    internal class Creature
+    {
+        private string name;
+        private int level;
+
+        public Creature(string name, int level = 1)
+        {
+            this.name = name;
+            this.level = level;
+        }
+        public Creature()
+        {
+        }
+        public string Info()
+        {
+            return $"{name}, Level {level}";
+        }
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {name}, my level is {level}.");
+        }
+
+    }
+}
