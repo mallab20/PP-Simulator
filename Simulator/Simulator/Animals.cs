c002bd9849187ba526a102c1c40bd02c37ce2e9d
Małgorzata Łabuzek <malgorzatalabuzek0808@gmail.com>
2024-10-30 23:06:44 +0100

    Settery

--- Simulator/Simulator/Animals.cs
+++ Simulator/Simulator/Animals.cs
@@ -10,12 +10,41 @@ namespace Simulator
 {
     public class Animals
     {
+
+        private string _description = "Unknown";
+
         public required string Description { get; init; }
         public uint Size { get; set; } = 3;
 
         public void Info()
         {
             Console.WriteLine($"{Description} <{Size}>");
+        }
+        private string ValidateDescription(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                input = "Unknown";
+            }
+
+            input = input.Trim();
+
+            if (input.Length > 15)
+            {
+                input = input.Substring(0, 15).TrimEnd();
+            }
+
+            if (input.Length < 3)
+            {
+                input = input.PadRight(3, '#');
+            }
+
+            if (char.IsLower(input[0]))
+            {
+                input = char.ToUpper(input[0]) + input.Substring(1);
+            }
+
+            return input;
         }
     }
 }
--- Simulator/Simulator/Creature .cs
+++ Simulator/Simulator/Creature .cs
@@ -1,33 +1,140 @@
-﻿using System;
+﻿using Simulator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Simulator
 {
-    internal class Creature
+    public class Creature
     {
         private string name;
         private int level;
 
-        public Creature(string name, int level = 1)
+
+        public string Name
         {
-            this.name = name;
-            this.level = level;
+            get { return name; }
+            set
+            {
+                name = CheckString(value);
+            }
         }
+
+        public int Level
+        {
+            get { return level; }
+            set
+            {
+                level = CheckInt(value);
+
+            }
+        }
+
+        public Creature(string? name = "Unknown", int level = 1)
+        {
+
+            this.Name = name;
+            this.Level = level;
+        }
+
         public Creature()
         {
+            this.Name = "Unknown";
+            this.Level = 1;
         }
-        public string Info()
+
+        private string CheckString(string inputName)
         {
-            return $"{name}, Level {level}";
+            if (string.IsNullOrWhiteSpace(inputName))
+            {
+                inputName = "Unknown";
+            }
+
+            inputName = inputName.Trim();
+
+            if (inputName.Length > 25)
+            {
+                inputName = inputName.Substring(0, 25).TrimEnd();
+            }
+
+            if (inputName.Length < 3)
+            {
+                inputName = inputName.PadRight(3, '#');
+            }
+
+            if (char.IsLower(inputName[0]))
+            {
+                inputName = char.ToUpper(inputName[0]) +
+                inputName.Substring(1);
+            }
+
+            return inputName;
+        }
+
+        private int CheckInt(int inputValue)
+        {
+            if (inputValue < 1)
+            {
+                inputValue = 1;
+            }
+
+            if (inputValue > 10)
+            {
+                inputValue = 10;
+            }
+
+            return inputValue;
+        }
+
+        public string Info
+        {
+
+            get
+            {
+
+
+                string outputValue = string.Empty;
+
+                outputValue = CheckString(this.Name);
+
+                int outputInt = 0;
+
+                outputInt = CheckInt(this.Level);
+
+                return $"{outputValue}, Level {outputInt}";
+
+            }
+
         }
 
         public void SayHi()
         {
-            Console.WriteLine($"Hi, I'm {name}, my level is {level}.");
+            Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
         }
 
+        public int Upgrade(int localLevel)
+        {
+            localLevel = localLevel + 1;
+
+            if (localLevel > 10)
+            {
+                localLevel = 10;
+            }
+
+            return localLevel;
+        }
+        public void Upgrade()
+        {
+            Level = Level + 1;
+
+            if (Level > 10)
+            {
+                Level = 10;
+            }
+
+        }
     }
 }
